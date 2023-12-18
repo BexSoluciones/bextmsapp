ee2f66f12066ce74a8476138f59d78773aec3116
Heider Zapa <heider.zapa@bexsoluciones.com>
2023-12-18 11:05:29 -0500

    fix database

--- lib/src/data/datasources/local/app_database.dart
+++ lib/src/data/datasources/local/app_database.dart
@@ -340,9 +340,6 @@ class AppDatabase {
     ''',
     '''
       ALTER TABLE $tableProcessingQueues ADD COLUMN ${ProcessingQueueFields.relation} INTEGER DEFAULT NULL
-    ''',
-    '''
-      ALTER TABLE $tableLocations ADD COLUMN ${LocationFields.send} INTEGER DEFAULT 0
     '''
   ];
 
